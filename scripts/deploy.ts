1525e8d52cbb6bf6790ff8da756c1062775bf3e3
web3ten0 <web3ten0@gmail.com>
2022-03-12 15:20:12 +0900

    exec Hoge.hoge()

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,8 +3,10 @@ import {ethers} from "hardhat";
 async function main() {
     const Hoge = await ethers.getContractFactory("Hoge");
     const hoge = await Hoge.deploy();
+    console.log(hoge.deployTransaction);
     await hoge.deployed();
     console.log("Hoge deployed to:", hoge.address);
+    console.log(await hoge.hoge());
 }
 
 main().catch(err => {
