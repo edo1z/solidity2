587e510e8541bc85c5136af366376e5b12309d4f
web3ten0 <web3ten0@gmail.com>
2022-03-12 21:01:10 +0900

    crated faucet contract

--- contracts/Hoge.sol
+++ contracts/Hoge.sol
@@ -1,8 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.7;
 
 contract Hoge {
-    uint num = 10;
-    function hoge() public view returns (uint) {
-        return num;
+    address payable admin;
+
+    receive() external payable {}
+
+    constructor() {
+        admin = payable(msg.sender);
+    }
+
+    function balance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function getEther(uint amount) public payable {
+        require(amount <= 1000000000000000000);
+        admin.transfer(amount);
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,13 +3,44 @@ import {ethers} from "hardhat";
 async function main() {
     const Hoge = await ethers.getContractFactory("Hoge");
     const hoge = await Hoge.deploy();
-    console.log(hoge.deployTransaction);
     await hoge.deployed();
     console.log("Hoge deployed to:", hoge.address);
-    console.log(await hoge.hoge());
+    const [owner] = await ethers.getSigners();
+    getContractBalance(hoge);
+    getOwnerBalance(owner);
+    const amount = ethers.utils.parseEther("1.0");
+    const tx = {
+        to: hoge.address,
+        value: amount
+    };
+    await owner.sendTransaction(tx);
+    hr();
+    console.log("owner send ether to contract.");
+    getContractBalance(hoge);
+    getOwnerBalance(owner);
+    const getAmount = ethers.BigNumber.from("50000000000000000");
+    await hoge.getEther(getAmount);
+    hr();
+    console.log("owner get ether from contract.");
+    getContractBalance(hoge);
+    getOwnerBalance(owner);
 }
 
 main().catch(err => {
     console.error(err);
     process.exitCode = 1;
 })
+
+async function getContractBalance(hoge: any) {
+    const balance = await hoge.balance();
+    console.log("Contract Balance:", balance);
+}
+
+async function getOwnerBalance(owner: any) {
+    const balance = await owner.getBalance();
+    console.log("Owner Balance:", balance);
+}
+
+function hr() {
+    console.log("--------------------------");
+}
